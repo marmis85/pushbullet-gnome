1895318ac81b59659d38bafff5822da9b715276d
marmis85 <marmis85@gmail.com>
2015-04-08 22:32:37 +0200

    removed cheklist and addresses pushes because they are no longer in the API and the official clients. also removed updating notifications because without checklists you only get updates for dismissed notifications.

--- pushbullet-gnome@stephencoakley.com/notifications.js
+++ pushbullet-gnome@stephencoakley.com/notifications.js
@@ -3,7 +3,6 @@ const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Main = imports.ui.main;
 const MessageTray = imports.ui.messageTray;
-const St = imports.gi.St;
 
 const Pushbullet = imports.misc.extensionUtils.getCurrentExtension();
 
@@ -29,12 +28,11 @@ const NotificationSource = new Lang.Class({
     },
 
     handlePush: function(push) {
+        this.dismiss(push)
+
         if (push.active && !push.dismissed) {
-            if (this._notifications.has(push.iden))
-                this.updatePush(push);
-            else this.showPush(push);
+            this.showPush(push);
         }
-        else this.dismiss(push);
     },
 
     showPush: function(push) {
@@ -43,12 +41,6 @@ const NotificationSource = new Lang.Class({
         }
         else if (push.type == "link") {
             this.showLinkPush(push);
-        }
-        else if (push.type == "address") {
-            this.showAddressPush(push);
-        }
-        else if (push.type == "checklist") {
-            this.showChecklistPush(push);
         }
         else if (push.type == "file") {
             this.showFilePush(push);
@@ -65,16 +57,6 @@ const NotificationSource = new Lang.Class({
         this._showNotification(push, notification);
     },
 
-    showAddressPush: function(push) {
-        let notification = new MessageTray.Notification(this._source, "Dummy", "Address");
-        this._showNotification(push, notification);
-    },
-
-    showChecklistPush: function(push) {
-        let notification = new MessageTray.Notification(this._source, "Dummy", "Checl List");
-        this._showNotification(push, notification);
-    },
-
     showFilePush: function(push) {
         let notification = new MessageTray.Notification(this._source, push.title, push.file_name);
         notification.addAction("Download File", Lang.bind(this, this.openFile, push.file_url));
@@ -84,50 +66,6 @@ const NotificationSource = new Lang.Class({
     _showNotification: function(push, notification) {
         this._source.notify(notification);
         this._notifications.set(push.iden, notification);
-    },
-
-    updatePush: function(push) {
-        if (push.type == "note") {
-            this.updateNotePush(push);
-        }
-        else if (push.type == "link") {
-            this.updateLinkPush(push);
-        }
-        else if (push.type == "address") {
-            this.updateAddressPush(push);
-        }
-        else if (push.type == "checklist") {
-            this.updateChecklistPush(push);
-        }
-        else if (push.type == "file") {
-            this.updateFilePush(push);
-        }
-    },
-
-    updateNotePush: function(push) {
-        let notification = this._notifications.get(push.iden);
-        if (notification) {
-            notification.update(push.title, push.body);
-        }
-    },
-
-    updateLinkPush: function(push) {
-        let notification = this._notifications.get(push.iden);
-        if (notification) {
-            notification.update(push.title, push.url);
-        }
-    },
-
-    updateAddressPush: function(push) {},
-
-    updateChecklistPush: function(push) {},
-
-    updateFilePush: function(push) {
-        let notification = this._notifications.get(push.iden);
-        if (notification) {
-            notification.update(push.title, push.file_name, { clear: true });
-            notification.addAction("Download File", Lang.bind(this, this.openFile, push.file_url));
-        }
     },
 
     dismiss: function(push) {
